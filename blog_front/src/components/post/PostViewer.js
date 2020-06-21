9286711be614cbbb01a3b24b37e641cf979e552b
yeongbi choe <cyeongb@gmail.com>
2020-06-21 16:39:22 +0900

    0621 16:39

--- blog_front/src/components/post/PostViewer.js
+++ blog_front/src/components/post/PostViewer.js
@@ -32,12 +32,16 @@ span+span:before {
 `;
 
 const Tags = styled.div`
-  margin-top: inline-block;
-  color: ${palette.cyan[7]};
-  text-decoration: none;
-  margin-right: 0.5rem;
-  &:hover {
-    color: ${palette.cyan[6]};
+  margin-top: 0.5rem;
+
+  .tag {
+    display: inline-block;
+    color: ${palette.cyan[7]};
+    text-decoration: none;
+    margin-right: 0.5rem;
+    &:hover {
+      color: ${palette.cyan[6]};
+    }
   }
 `;
 
--- blog_front/src/containers/post/PostViewerContainer.js
+++ blog_front/src/containers/post/PostViewerContainer.js
@@ -4,15 +4,16 @@ import { withRouter } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 
-const PostViewerContainer = ({match}) => {
-//처음 마운트 될 때 post read API를 요청한다.
-const {postId} = match.params;
-
-
-
+const PostViewerContainer = ({ match }) => {
+  //처음 마운트 될 때 post read API를 요청한다.
+  const { postId } = match.params;
+  const dispatch = useDispatch();
+  const { post, error, loading } = useSelector(({ post, loading }) => ({
+    post: post.post,
+    error: post.error,
+    laoding: loading['post/READ_POST'],
+  }));
 };
-    return
-    
-    
+return;
 
 export default withRouter(PostViewerContainer);
